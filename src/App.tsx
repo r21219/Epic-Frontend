858b6df18ef3c3b2430e0539f4f68375510da92d
Michal Hrton <45671548+michalh17@users.noreply.github.com>
2023-04-20 15:48:33 +0200

    Tasky se zobrazují normálně, ale ještě ne podle návrhu. Lze přidávat title, deadline a kategorii (výpis z kategorií se zobrazí jako modální okno). Při kliknutí na Add Task se přidá task do konkrétní kategorie (po refreshnutí se vymaže). Pokud něco není vyplněno, task se nepřidá a nevyplněné inputy se obarví červeně. //TODO: Modální okno výběru kategorie je zvláštní, má pouze tlačítko close a radio buttony (matoucí). Vybraná kategorie se nezobrazí vedle tlačítka, takže uživatel neví, co si vybral. Zároveň pokud není vybrána kategorie, sice se nepovede přidat task, ale není žádné značení, které by o tom vypovídalo.

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Route, Routes} from "react-router-dom";
 import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
 import {
   Container,
   Row,
   Col,
   Accordion,
   Card,
-  Form, Button,
+  Form, Button, Modal,
 } from "react-bootstrap";
 import "./App.css";
 
@@ -101,6 +104,8 @@ const renderTasks = (tasks: Task[]) => {
 
   return tasks.map((task, index) => (
       <Card key={task.id} className="task-row">
+        <Row>
+          <Col>
         <Form.Check
             type="checkbox"
             id={`task-checkbox-${task.id}`}
@@ -110,11 +115,19 @@ const renderTasks = (tasks: Task[]) => {
               // Handle checkbox change event
             }}
         />
+          </Col>
+            <Col>
         <Form.Label htmlFor={`task-checkbox-${task.id}`} className="task-title">
           {task.title}
         </Form.Label>
+            </Col>
+          <Col>
         <span className="task-date">{task.date}</span>
+            </Col>
+            <Col>
         <span className="task-category">{task.category}</span>
+            </Col>
+            <Col>
         <Button
             className="task-delete"
             onClick={() => {
@@ -123,16 +136,112 @@ const renderTasks = (tasks: Task[]) => {
         >
           Delete
         </Button>
+            </Col>
+        </Row>
       </Card>
   ));
 };
 
 const App: React.FC = () => {
+  // Search Bar
   const [searchTerm, setSearchTerm] = useState("");
+
+    // Categories
   const [categories, setCategories] = useState<Array<Category>>(sampleCategories);
+
+  // New Task Form ověřuje vstupní data
+  const [taskTitle, setTaskTitle] = useState("");
+  const [deadline, setDeadline] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState<Category | null>(null);
+  const [inputValidation, setInputValidation] = useState({
+    taskTitle: true,
+    deadline: true,
+    category: true,
+  });
 
   // ...other useState hooks, functions and event handlers
   //const [searchTerm, setSearchTerm] = useState("");
+
+  //  TASK ADDING
+  const handleTaskTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTaskTitle(e.target.value);
+  };
+
+  const handleDeadlineChange = (date: Date | null) => {
+    if (date) {
+      setDeadline(date.toISOString().split("T")[0]);
+    } else {
+      setDeadline("");
+    }
+  };
+
+
+  const handleCategorySelect = (category: Category) => {
+    setSelectedCategory(category);
+  };
+
+  // Modal for adding new category visibility
+  const [categoryModalOpen, setCategoryModalOpen] = useState(false);
+
+
+  const isFormValid = () => {
+    const taskTitleValid = taskTitle.trim().length > 0;
+    const deadlineValid = deadline.length > 0;
+    const categoryValid = selectedCategory !== null;
+
+    setInputValidation({
+      taskTitle: taskTitleValid,
+      deadline: deadlineValid,
+      category: categoryValid,
+    });
+
+    return taskTitleValid && deadlineValid && categoryValid;
+  };
+
+  const addTask = () => {
+
+      if (!isFormValid()) return;
+
+      // Find the index of the selected category in the categories array
+      const categoryIndex = categories.findIndex(
+          (category) => category.id === selectedCategory?.id
+      );
+
+      if (categoryIndex !== -1 && selectedCategory) {
+        // Generate a unique task ID
+        const newTaskId =
+            Math.max(...categories[categoryIndex].tasks.map((task) => task.id)) + 1;
+
+        // Create a new task object
+        const newTask: Task = {
+          id: newTaskId,
+          title: taskTitle,
+          date: deadline,
+          category: selectedCategory.name,
+          completed: false,
+        };
+
+        // Add the new task to the tasks array of the selected category
+        const updatedCategories = [...categories];
+        updatedCategories[categoryIndex].tasks = [
+          ...updatedCategories[categoryIndex].tasks,
+          newTask,
+        ];
+
+        // Update the categories state
+        setCategories(updatedCategories);
+
+        // Clear the input fields
+        setTaskTitle("");
+        setDeadline("");
+        setSelectedCategory(null);
+      }
+
+
+  };
+
+  // END OF ADDING TASK
+
 
   const handleSearchInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(e.target.value);
@@ -160,7 +269,76 @@ const App: React.FC = () => {
           </Col>
         </Row>
         {/* New Task Form */}
-        {/* ... */}
+        {/* Add Task Input */}
+        <Row className="add-task-input fixed-bottom">
+          <Col>
+            <Form onSubmit={(e) => {
+              e.preventDefault();
+              addTask();
+            }}>
+              <Row>
+                <Col>
+                  <Form.Control
+                      type="text"
+                      placeholder="Task title"
+                      value={taskTitle}
+                      onChange={handleTaskTitleChange}
+                      isInvalid={!inputValidation.taskTitle}
+                  />
+                </Col>
+                <Col>
+                  {/* Date Picker */}
+                  <DatePicker
+                      selected={deadline ? new Date(deadline) : null}
+                      onChange={handleDeadlineChange}
+                      dateFormat="yyyy-MM-dd"
+                      placeholderText="Deadline"
+                      className={`form-control ${!inputValidation.deadline ? "is-invalid" : ""}`}
+                  />
+
+                </Col>
+                <Col>
+                  {/* Category Selector */}
+                  {/* Category Selector */}
+                  <Button onClick={() => setCategoryModalOpen(true)}>Category</Button>
+                  <Modal
+                      show={categoryModalOpen}
+                      onHide={() => setCategoryModalOpen(false)}
+                      centered
+                  >
+                    <Modal.Header closeButton>
+                      <Modal.Title>Select Category</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                      <Form>
+                        {categories.map((category) => (
+                            <Form.Check
+                                key={category.id}
+                                type="radio"
+                                id={`category-radio-${category.id}`}
+                                label={category.name}
+                                name="category"
+                                onClick={() => handleCategorySelect(category)}
+                            />
+                        ))}
+                      </Form>
+                    </Modal.Body>
+                    <Modal.Footer>
+                      <Button variant="secondary" onClick={() => setCategoryModalOpen(false)}>
+                        Close
+                      </Button>
+                    </Modal.Footer>
+                  </Modal>
+
+                </Col>
+                <Col>
+                  <Button onClick={addTask}>Add Task</Button>
+                </Col>
+              </Row>
+            </Form>
+          </Col>
+        </Row>
+
       </Container>
   );
 };
